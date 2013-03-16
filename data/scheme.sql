1aaaeec1abd436bda9fa4bc1ba53694a38b2cd3c
Andrey Koltochnik <ankolt@gmail.com>
2013-03-16 23:30:45 +0600

    новая структура БД

--- data/scheme.sql
+++ data/scheme.sql
@@ -1,15 +1,34 @@
-CREATE TABLE allmusic (
-  id                    INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id),
-  uid                   VARCHAR(100),
-  song_name             VARCHAR(400),
-  mobile_rate           DECIMAL(6, 6),
-  public_rate           DECIMAL(6, 6),
-  composer              VARCHAR(400),
-  artist                VARCHAR(400),
-  publisher             VARCHAR(400),
-  comment               VARCHAR(600),
-  controlled_mech_share DECIMAL(6, 6),
-  collect_mech_share    DECIMAL(6, 6)
+# CREATE TABLE allmusic (
+#   id                    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+#   uid                   VARCHAR(100),
+#   song_name             VARCHAR(400),
+#   mobile_rate           DECIMAL(6, 6),
+#   public_rate           DECIMAL(6, 6),
+#   composer              VARCHAR(400),
+#   artist                VARCHAR(400),
+#   publisher             VARCHAR(400),
+#   comment               VARCHAR(600),
+#   controlled_mech_share DECIMAL(6, 6),
+#   collect_mech_share    DECIMAL(6, 6)
+# );
+
+CREATE TABLE catalog (
+  id      INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name    VARCHAR(200),
+  royalty DECIMAL(6, 6)
 );
+
+
+CREATE TABLE composition (
+  id         INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  catalog_id INT NOT NULL,
+  code       VARCHAR(20),
+  name       VARCHAR (400),
+  artist     VARCHAR (400),
+  composer   VARCHAR(400),
+  shareMobile DECIMAL(6, 6),
+  sharePublic DECIMAL(6, 6)
+);
+
 
 
