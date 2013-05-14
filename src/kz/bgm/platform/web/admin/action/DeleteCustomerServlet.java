1e0f0b633c1f4e73271c7e9cd54d40dcb0b4f4ca
max-antp <max.freelance.kz@gmail.com>
2013-05-15 00:37:09 +0600

    ции

--- src/kz/bgm/platform/web/admin/action/DeleteCustomerServlet.java
+++ src/kz/bgm/platform/web/admin/action/DeleteCustomerServlet.java
@@ -21,6 +21,7 @@ public class DeleteCustomerServlet extends HttpServlet {
         storage = CatalogFactory.getStorage();
     }
 
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String StrCustomerId = req.getParameter("customer-id");
