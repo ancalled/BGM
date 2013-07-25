570fce5f1fdf594668cc63cc2eeb76f86a15bb79
ancalled <ankolt@gmail.com>
2013-07-25 16:00:55 +0600

    Интерфейс загрузки клиентского отчета 7

---
+++ data/patch2.sql
@@ -0,0 +1,4 @@
+ALTER TABLE customer CHANGE authorRoyalty authorRoyalty FLOAT;
+ALTER TABLE customer CHANGE relatedRoyalty relatedRoyalty FLOAT;
+
+ALTER TABLE customer_report_item CHANGE price price FLOAT;
