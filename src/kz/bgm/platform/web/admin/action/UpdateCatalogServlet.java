6637e1cddf7407b6c608da8864bb4bf85b0c1d4e
ancalled <ankolt@gmail.com>
2013-11-21 09:21:06 +0600

    Тестовый комит

--- src/kz/bgm/platform/web/admin/action/UpdateCatalogServlet.java
+++ src/kz/bgm/platform/web/admin/action/UpdateCatalogServlet.java
@@ -15,7 +15,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -121,6 +120,7 @@ public class UpdateCatalogServlet extends HttpServlet {
             jsonObj.put("status", update.getStatus() == Status.OK ? "ok" : "warn");
             jsonObj.put("redirect", RESULT_URL + "?id=" + update.getId());
             jsonObj.writeJSONString(out);
+
 
 
         } catch (Exception e) {
